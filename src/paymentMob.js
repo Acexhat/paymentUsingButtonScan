a98bd0bac2b72a2d6af363c32e3b6db9561bd66c
akshatBhaskar <akshat.bhaskar@theflyy.com>
2022-03-30 02:19:38 +0530

    phonePe integrayion

--- src/paymentMob.js
+++ src/paymentMob.js
@@ -10,12 +10,16 @@ export default function PaymentMob() {
         currency: 'INR',
     })
 
+    const handlePhonepe = () => {
+        window.location.href = " upi://pay?pa=bhaskar03112000@okhdfcbankx&pn=AkshatBhaskar&am=1.00&mam=2.00&tid=YBLc6f12c2333b2495fbfd024b12ad43dc7&tr=T2002061921587731419308&tn=Payment%20for%20TX117785240954814000&mc=5311&mode=04&purpose=00"
+    }
+
     const handlePayRoute = (UPI_APP_NAME) => {
         let paymentURL = '';
         let random_trxn_no = (Math.random() + 1).toString(36).substring(2);
         switch (UPI_APP_NAME) {
             case 'GOOGLE': paymentURL = `tez://upi/pay?pa=${paymentDetails.upiId}&pn=${paymentDetails.name}&mc=&tr=${random_trxn_no}&am=${paymentDetails.amount}&cu=${paymentDetails.currency}`; break;
-            case 'PHONE_PAY': paymentURL = `phonepe://pay?pa=${paymentDetails.upiId}&pn=${paymentDetails.name}&am=1&cu=INR`; break;
+            // case 'PHONE_PAY': paymentURL = `phonepe://pay?pa=${paymentDetails.upiId}&pn=${paymentDetails.name}&am=1&cu=INR`; break;
             case 'PAYTM': paymentURL = `paytmmp://pay?pa=${paymentDetails.upiId}&pn=${paymentDetails.name}&am=1&cu=INR`; break;
             case 'ANY': paymentURL = `upi://pay?pa=9262561881@ybl&pn=Akshat Bhaskar&am=1&tn=442389749fhefh&cu=INR`; break;
             // upi://pay?pa=bhaskar03112000@okhdfcbank&amp;pn=Akshat&amp;cu=INR
@@ -30,7 +34,7 @@ export default function PaymentMob() {
                 <button className="btn" onClick={() => handlePayRoute('GOOGLE')}>
                     Google Pay
                 </button>
-                <button className="btn" onClick={() => handlePayRoute('PHONE_PAY')}>
+                <button className="btn" onClick={() => handlePhonepe()}>
                     PhonePe
                 </button>
                 <button className="btn" onClick={() => handlePayRoute('PAYTM')}>
