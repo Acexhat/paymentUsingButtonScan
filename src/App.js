bcb7251a5ea47d9b8438bc3bc3197caec605e08f
akshatBhaskar <akshat.bhaskar@theflyy.com>
2022-03-30 00:35:28 +0530

    gpay fix-2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import QRCode from "qrcode.react";
 
 function App() {
 
   const [paymentDetails, setPaymentDetails] = React.useState({
     name: 'Akshat Bhaskar',
-    upiId: 'bhaskar03112000@okhdfcbank',
+    upiId: '9262561881@ybl',
     amount: '1',
     currency: 'INR',
   })
+
+  const [Qrvalue, setQrvalue] = React.useState('upi://pay?pa=9262561881@ybl&amp;pn=JOHN BRITAS AK &amp;cu=1');
 
   const handlePayRoute = (UPI_APP_NAME) => {
     let paymentURL = '';
@@ -20,9 +23,11 @@ function App() {
       default: break;
     }
     let random_trxn_no = (Math.random() + 1).toString(36).substring(2);
-    paymentURL = paymentURL + `pay?pa=${paymentDetails.upiId}&pn=${paymentDetails.name}&mc=0000&tr=${random_trxn_no}&tn=HelloWorld&am=${paymentDetails.amount}&cu=${paymentDetails.currency}`;
+    paymentURL = paymentURL + `pay?pa=${paymentDetails.upiId}&pn=${paymentDetails.name}&mc=0001&tr=${random_trxn_no}&am=${paymentDetails.amount}&cu=${paymentDetails.currency}`;
     window.location.href = paymentURL;
   }
+
+
 
   return (
     <div className="App">
@@ -35,7 +40,7 @@ function App() {
       <button onClick={() => handlePayRoute('PAYTM')}>
         Paytm
       </button>
-
+      {/* <QRCode value={Qrvalue} size={256} /> */}
     </div>
   );
 }
